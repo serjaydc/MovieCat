1aded2f9f970ff8b5f282d9f6c548fefce0b34ed
Serjay <serhiinote@gmail.com>
2026-02-14 20:13:06 +0000

    Deploy v2.4

--- backend/src/app.py
+++ backend/src/app.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from .config import Config
 from .extensions import db, jwt, bcrypt
-
 from .routes.routes import auth_routes, tmdb_routes, list_routes
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,4 +18,6 @@ jwt.init_app(app)
 bcrypt.init_app(app)
 
 with app.app_context():
+    instance_path = os.path.join(os.path.dirname(__file__), "instance")
+    os.makedirs(instance_path, exist_ok=True)
     db.create_all()
--- backend/src/config.py
+++ backend/src/config.py
@@ -8,10 +8,11 @@ class Config:
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "dev-jwt-secret")
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=7)
 
-    SQLALCHEMY_DATABASE_URI = ("sqlite:///" + os.path.join(BASE_DIR, "instance", "app.db"))
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
+        BASE_DIR, "instance", "app.db"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     TMDB_BASE_URL = "https://api.themoviedb.org/3"
     TMDB_API_KEY = os.getenv("TMDB_API_KEY")
     TMDB_ACCESS_TOKEN = os.getenv("TMDB_ACCESS_TOKEN")
-
