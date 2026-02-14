645eaa32cb5935a396617eaf16c231ba490691cb
Serjay <serhiinote@gmail.com>
2026-02-14 20:16:59 +0000

    Deploy v2.5

--- frontend/src/js/api/api.js
+++ frontend/src/js/api/api.js
@@ -1,3 +1,7 @@
-export const apiAuth = "http://127.0.0.1:5000/api/auth";
-export const apiTMDB = "http://127.0.0.1:5000/api/tmdb";
-export const apiList = "http://127.0.0.1:5000/api/list";
+// export const apiAuth = "http://127.0.0.1:5000/api/auth";
+// export const apiTMDB = "http://127.0.0.1:5000/api/tmdb";
+// export const apiList = "http://127.0.0.1:5000/api/list";
+
+export const apiAuth = "https://moviecat.onrender.com/api/auth";
+export const apiTMDB = "https://moviecat.onrender.com/api/tmdb";
+export const apiList = "https://moviecat.onrender.com/api/list";
